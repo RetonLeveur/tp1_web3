5026a2e089b82471d43016cb0be955ae6f0f6ee8
1972174 <74424796+1972174@users.noreply.github.com>
2022-10-16 13:16:20 -0400

    ajout fonction pour leaderboard

--- src/components/leaderBoard.js
+++ src/components/leaderBoard.js
@@ -1,31 +1,41 @@
+let leaderBoard = [
+  { name: "john", score: 55 },
+  { name: "john", score: 35 },
+  { name: "john", score: 26 },
+  { name: "john", score: 56 },
+  { name: "john", score: 60 },
+  { name: "john", score: 44 },
+  { name: "john", score: 37 },
+  { name: "john", score: 22 },
+  { name: "john", score: 57 },
+  { name: "john", score: 52 },
+];
 
-let leaderBoard=[
-    {name:'john',score:555},
-    {name:'john',score:555}, 
-    {name:'john',score:555},
-    {name:'john',score:555},
-    {name:'john',score:555}, 
-    {name:'john',score:555},
-    {name:'john',score:555},
-    {name:'john',score:555},
-    {name:'john',score:555}
-]
+export function createLeaderBoard() {
+  const board = $("<div></div>")
+    .addClass("leaderBoard")
+    .attr("id", "leaderBoard");
+  board.append($("<h2>Top 10 Players</h2>").addClass("leaderTitle"));
+  let position = 1;
+  leaderBoard.sort((a, b) => b.score - a.score);
+  leaderBoard.forEach((entry) => {
+    board.append(
+      $("<p></p>")
+        .addClass("leaderEntry")
+        .text(position + ":" + entry.name + "    " + entry.score)
+    );
+    position += 1;
+  });
 
-export function createLeaderBoard(){
-   const board=  $('<div></div>').addClass('leaderBoard').attr('id','leaderBoard');
-   board.append($('<h2>Top 10 Players</h2>').addClass('leaderTitle'));
-   let position =1
-   leaderBoard.forEach(entry => {
-  
-   board.append($('<p></p>').addClass('leaderEntry').text(position+':'+entry.name +'    '+entry.score));
-   position+=1; 
-   });
-
-   return board;
+  return board;
 }
 
-export function addToLeaderBoard(){
-    //TODO Ajouter un nouveau record dans le leaderboard
+export function addToLeaderBoard(name, score) {
+  if (leaderBoard.length <= 10) {
+    if (leaderBoard.filter((a) => a.score < score)) {
+      leaderBoard.pop();
+      leaderBoard.push(name, score);
+    }
+  }
+  return leaderBoard.sort((a, b) => b.score - a.score);
 }
-
-
